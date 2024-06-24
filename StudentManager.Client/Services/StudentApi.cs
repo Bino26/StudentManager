363cf59153e9fede39c9896a040f816793f5fbe2
Bino26 <hbino1999@gmail.com>
2024-06-24 17:24:11 +0000

    🔧Fix Display Bug and Complete Project

---
+++ StudentManager.Client/Services/StudentApi.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentManager.Client.Services
+{
+    public class StudentApi
+    {
+        private readonly HttpClient httpClient;
+
+        public StudentApi(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task GetStudentById()
+        {
+
+        }
+    }
+}
